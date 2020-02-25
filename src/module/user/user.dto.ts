5c4b4d0f7f80ac64c85f336b3f9a66a946b2aa0f
hotfireeagle <hotfireeagle@163.com>
2020-02-26 00:08:33 +0800

    update::校验失败报错信息格式化

--- src/module/user/user.dto.ts
+++ src/module/user/user.dto.ts
@@ -1,12 +1,20 @@
-import { IsString, IsInt } from 'class-validator'
+import { IsString, IsInt, IsNotEmpty, MinLength, MaxLength, Min, Max } from 'class-validator'
 
 export class CreateUserDto {
   @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(8)
   firstName: string
 
   @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(8)
   lastName: string
 
   @IsInt()
+  @Min(1)
+  @Max(150)
   age: number
 }
--- src/pipe/validation.pipe.ts
+++ src/pipe/validation.pipe.ts
@@ -1,23 +1,38 @@
-import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common'
+import { PipeTransform, Injectable, ArgumentMetadata, HttpException, HttpStatus } from '@nestjs/common'
 import { validate } from 'class-validator'
 import { plainToClass } from 'class-transformer'
+import { ERROR_STATUS } from '../constant'
 
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
+
+  /** 实现pipe逻辑的方法 */
   async transform(value: any, { metatype }: ArgumentMetadata) {
-    if (!metatype || !this.toValidate(metatype)) {
+    if (!metatype || this.isInWhitelist(metatype)) {
       return value
     }
     const object = plainToClass(metatype, value)
     const errors = await validate(object)
     if (errors.length > 0) {
-      throw new BadRequestException('Validation failed')
+      const message = this.generateError(errors)
+      throw new HttpException({status: ERROR_STATUS, message}, HttpStatus.BAD_REQUEST)
     }
     return value
   }
 
-  private toValidate(metatype: Function): boolean {
-    const types: Array<Function> = [String, Boolean, Number, Array, Object]
+  /** 确定哪些数据无需被校验 */
+  private isInWhitelist(metatype: Function): boolean {
+    const types: Array<Function> = [String, Boolean, Number, Array, Object] // js的原生类型无需进行
     return types.includes(metatype)
+  }
+
+  /** 构造出错误提示数据 */
+  private generateError(errors: any[]): string {
+    const result = []
+    errors.map(errObj => {
+      const errDescObj = errObj.constraints
+      Object.keys(errDescObj).map(key => { result.push(errDescObj[key]) })
+    })
+    return result.join('-')
   }
 }
